890e70456f07fd31d74c24d830bb59c7dec04e5b
abdulhaseebss <m.saleemhaseeb@gmail.com>
2023-09-24 20:58:48 +0500

    Mobile App

--- cart.js
+++ cart.js
@@ -9,16 +9,43 @@ function backToHome() {
 const div = document.querySelector('#itemsDiv')
 const pera = document.querySelector('.pera')
 
-for (let i = 0; i < items.length; i++) {
-    div.innerHTML += `<div class="maindiv"><img src="./assets/${items[i].image}" alt="">
-    <h1>${items[i].brand} ${items[i].model}</br>
-    Quantity:<button class = "operator">-</button>${items[i].quantity}<button class = "operator">+</button></br>
-    <span class="price">Price: ${items[i].price}/= pkr</span></br>
-    <span class="total-price">Total Price: ${items[i].price * items[i].quantity}/= pkr</span></h1>
-    </div>`;
-    const grandTotal = items[i].price * items[i].quantity;
-    pera.innerHTML = `Grand Total = ${grandTotal}`
+
+function render() {
+    for (let i = 0; i < items.length; i++) {
+        div.innerHTML += `<div class="maindiv"><img src="./assets/${items[i].image}" alt="">
+        <h1>${items[i].brand} ${items[i].model}</br>
+        Quantity:<button class = "operator" onclick = "sumValue(${i})">-</button>${items[i].quantity}<button class = "operator" onclick="addValue(${i})">+</button></br>
+        <span class="price">Price: ${items[i].price}/= pkr</span></br>
+        <span class="total-price">Total Price: ${items[i].price * items[i].quantity}/= pkr</span></h1>
+        </div>`;
+        const grandTotal = items[i].price * items[i].quantity;
+        pera.innerHTML = `Grand Total = ${grandTotal}`
+    }
+    
 }
 
+render()
 
+
+
+function addValue(index) {
+    div.innerHTML = "";
+    items[index].quantity += 1;
+
+    render()
+}
+
+function sumValue(index) {
+    div.innerHTML = "";
+    items[index].quantity -= 1;
+
+    render()
+    if (items[index].quantity === 0){
+        div.innerHTML = "";
+        items.splice(index , 1)
+        render();
+    }
+      
+
+}
 
